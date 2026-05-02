ba3cbcc1379df604feb737d2bf1620da7c4eb332
hariasahil25 <hariyasahil1025@gmail.com>
2026-05-02 20:19:28 +0530

    Create fibonacci_module.py to generate Fibonacci series
    This module defines a function to generate and print the Fibonacci series up to n terms.

---
+++ fibonacci_module.py
@@ -0,0 +1,13 @@
+# Module to generate Fibonacci series
+
+def fibonacci(n):
+    a = 0
+    b = 1
+
+    print("Fibonacci Series:")
+
+    for i in range(n):
+        print(a, end=" ")
+        c = a + b
+        a = b
+        b = c
